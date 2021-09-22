838b45e76636a50271c1834ac8b5077eff1c6518
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-09-22 20:02:18 -0300

    quase completo

--- maquina.js
+++ maquina.js
@@ -1,48 +1,96 @@
-function escolher(){
-    let cash = document.getElementById('cash')
-    let res = document.getElementById('res')
-    let doce = document.getElementsByName('doce')
-    let  troco = Number(cash.value)
-    var doceS = ''
+var change = 0
+var dinInserido = 0
+var totalPago = 0
+var msg = ""
 
-    if(cash.value.length == 0){
+var doces = ["cupcake", "sorvete", "donut"]
+var preço = 8
 
-        window.alert('Dinheiro insuficiente')
+var mensagem1 = document.getElementById("mensagem")
 
-    } else {
+const currencyOne = 1
+const currencyTwo = 2
+const currencyFive = 5
 
-        if(doce[0].checked){
+function getTotal() {
+    var currencyOnes = Number(document.getElementById("1").value)
+    var currencyTwos = Number(document.getElementById("2").value)
+    var currencyFives = Number(document.getElementById("5").value)
 
-            doceS = 'Cupcake'
-            troco = troco - 6
-            if(troco < 0){
-                window.alert('Dinheiro insuficiente')
-            } else {
-                res.innerHTML = `Seu doce é ${doceS} e seu troco é ${troco}`
-            }
-
-        } else if (doce[1].checked){
-
-            doceS = 'Sorvete'
-            troco = troco - 7
-            if(troco < 0){
-                window.alert('Dinheiro insuficiente')
-            } else {
-                res.innerHTML = `Seu doce é ${doceS} e seu troco é ${troco}`
-            }
-
-        } else {
-
-            doceS = 'Donut'
-            troco = troco - 8
-            if(troco < 0){
-                window.alert('Dinheiro insuficiente')
-            } else {
-                res.innerHTML = `Seu doce é ${doceS} e seu troco é ${troco}`
-            }
-
-        }
-
+    if(currencyOnes > 0){
+        currencyOnes = currencyOnes * currencyOne
+    }
+    if(currencyTwos > 0){
+        currencyTwos = currencyTwos * currencyTwo
+    }
+    if(currencyFives > 0){
+        currencyFives = currencyFives * currencyFive
     }
 
+    totalPago = currencyOnes + currencyTwos + currencyFives
+
+    return totalPago.toFixed(2)
+}
+function contagem(){
+    dinInserido = getTotal();
+    document.getElementById("pago").innerHTML = dinInserido
+}
+function limpar(){
+    document.getElementById("1").value = 0
+    document.getElementById("2").value = 0
+    document.getElementById("5").value = 0
+    dinInserido = 0;
+    document.getElementById("pago").innerHTML = dinInserido
+}
+function cancelar(){
+    getTotal()
+    if(totalPago > 0 ){
+        msg = "Transação cancelada. $" + totalPago.toFixed(2) + " foi retornado."
+        mensagem1.innerHTML = msg;
+        limpar()
+    } else if (totalPago == 0) {
+        msg = "Insira o dinheiro..."
+        mensagem1.innerHTML = msg;
+        limpar()
+    }
+}
+function calculateChange(){
+    var tempChange = 0
+
+    if(getTotal() != 0) {
+        return(tempChange = (getTotal() - preço).toFixed(2))
+    }
+
+    return tempChange.toFixed(2)
+}
+function retirarDoce(doce){
+    mensagem1.innerHTML = ""
+    change = 0
+
+    var doceSelecionado = doces[doce]
+
+    change = calculateChange()
+
+    if (change < 0){
+        msg = "Dinheiro Insuficiente. $" + totalPago.toFixed(2) + " foi retornado."
+        totalPago = 0
+        change = 0
+        limpar()
+        mensagem1.innerHTML = msg;
+    } else if (change > 0){
+        msg = doceSelecionado + " foi retirado. $" + change + " foi retoranado."
+        totalPago = 0
+        change = 0
+        limpar()
+        mensagem1.innerHTML = msg;
+    } else if (totalPago == 0) {
+        msg = "Pague antes de selecionar o doce"
+        mensagem1.innerHTML = msg;
+    } else if (change == 0){
+        msg = doceSelecionado + " foi retirado."
+        totalPago = 0
+        change = 0
+        limpar()
+        mensagem1.innerHTML = msg;
+    }
 }
