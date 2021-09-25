d670f117afcb82b389292786c55daed9fc1bfcff
Vinicius Cezario Rodrigues <vcroficial.07@gmail.com>
2021-09-24 21:13:29 -0300

    completed js

--- maquina.js
+++ maquina.js
@@ -1,96 +1,125 @@
-var change = 0
-var dinInserido = 0
-var totalPago = 0
-var msg = ""
+var change = 0;
+var moneyInserted = 0;
+var msg = ";";
 
-var doces = ["cupcake", "sorvete", "donut"]
-var preço = 8
+var candys = ["Doce1", "Doce2", "Doce3"];
+const price1 = 6.00;
+const price2 = 7.00;
+const price3 = 8.00;
 
-var mensagem1 = document.getElementById("mensagem")
+var messageElement = document.getElementById("message")
 
-const currencyOne = 1
-const currencyTwo = 2
-const currencyFive = 5
+var totalPaid = 0;
+
+const currency_one = 1;
+const currency_two = 2;
+const currency_five = 5;
+
 
 function getTotal() {
-    var currencyOnes = Number(document.getElementById("1").value)
-    var currencyTwos = Number(document.getElementById("2").value)
-    var currencyFives = Number(document.getElementById("5").value)
-
-    if(currencyOnes > 0){
-        currencyOnes = currencyOnes * currencyOne
-    }
-    if(currencyTwos > 0){
-        currencyTwos = currencyTwos * currencyTwo
-    }
-    if(currencyFives > 0){
-        currencyFives = currencyFives * currencyFive
+    var currency_ones = Number(document.getElementById('ones').value);
+    var  currency_twos = Number(document.getElementById('two').value);
+    var currency_fives = Number(document.getElementById('fives').value);
+    
+    if (currency_ones > 0) {
+        currency_ones = currency_ones * currency_one;
     }
 
-    totalPago = currencyOnes + currencyTwos + currencyFives
+    if(currency_twos > 0) {
+        currency_twos = currency_twos * currency_two;
+    }
 
-    return totalPago.toFixed(2)
+    if (currency_fives > 0) {
+        currency_fives = currency_fives * currency_five;
+    }
+   
+    totalPaid = currency_ones + currency_twos + currency_fives;
+    return totalPaid.toFixed(2);
+} 
+
+function tally() {
+    moneyInserted = getTotal();
+    document.getElementById("paid").innerHTML = moneyInserted;
 }
-function contagem(){
-    dinInserido = getTotal();
-    document.getElementById("pago").innerHTML = dinInserido
+
+function clearTally() {
+    moneyInserted = 0;
+    document.getElementById("paid").innerHTML = moneyInserted;
 }
-function limpar(){
-    document.getElementById("1").value = 0
-    document.getElementById("2").value = 0
-    document.getElementById("5").value = 0
-    dinInserido = 0;
-    document.getElementById("pago").innerHTML = dinInserido
+
+function clearForm() {
+    document.getElementById("ones").value = 0;
+    document.getElementById("two").value = 0;
+    document.getElementById("fives").value = 0;
 }
-function cancelar(){
-    getTotal()
-    if(totalPago > 0 ){
-        msg = "Transação cancelada. $" + totalPago.toFixed(2) + " foi retornado."
-        mensagem1.innerHTML = msg;
-        limpar()
-    } else if (totalPago == 0) {
-        msg = "Insira o dinheiro..."
-        mensagem1.innerHTML = msg;
-        limpar()
+
+function cancel() {
+    getTotal();
+
+    if(totalPaid > 0) {
+        msg = "Transaction cancelled. $" +totalPaid.toFixed(2) + " has been returned" 
+        clearForm();
+        clearTally();
+
+        messageElement.innerHTML = msg;
+    } else if(totalPaid == 0) {
+        msg = "Insert money first. Select candy";
+
+        messageElement.innerHTML = msg;
     }
 }
-function calculateChange(){
-    var tempChange = 0
 
+function calculateChange(candy) {
+    var tempChange = 0;
+
+    // return (tempChange = (getTotal() - price).toFixed(2));
     if(getTotal() != 0) {
-        return(tempChange = (getTotal() - preço).toFixed(2))
+        if(candy == "Doce1") {
+            return (tempChange = (getTotal() - price1).toFixed(2));
+        } else if(candy ==  "Doce2") {
+            return (tempChange = (getTotal() - price2).toFixed(2));
+        } else {
+            return (tempChange = (getTotal() - price3).toFixed(2));
+        }
     }
-
-    return tempChange.toFixed(2)
+    return tempChange.toFixed(2);
 }
-function retirarDoce(doce){
-    mensagem1.innerHTML = ""
-    change = 0
 
-    var doceSelecionado = doces[doce]
+function dispenseCandy(candy) {
+    
+    var  selectedCandy = candys[candy];
 
-    change = calculateChange()
+    change = calculateChange(candys[candy]);
 
-    if (change < 0){
-        msg = "Dinheiro Insuficiente. $" + totalPago.toFixed(2) + " foi retornado."
-        totalPago = 0
-        change = 0
-        limpar()
-        mensagem1.innerHTML = msg;
-    } else if (change > 0){
-        msg = doceSelecionado + " foi retirado. $" + change + " foi retoranado."
-        totalPago = 0
-        change = 0
-        limpar()
-        mensagem1.innerHTML = msg;
-    } else if (totalPago == 0) {
-        msg = "Pague antes de selecionar o doce"
-        mensagem1.innerHTML = msg;
-    } else if (change == 0){
-        msg = doceSelecionado + " foi retirado."
-        totalPago = 0
-        change = 0
-        limpar()
-        mensagem1.innerHTML = msg;
+    console.log(change)
+
+    if(change < 0) {
+        msg = "Você não me deu dinheiro suficiente. R$" + totalPaid.toFixed(2) + " retornado!";
+
+        totalPaid = 0;
+        change = 0;
+        clearForm();
+        clearTally();
+
+        messageElement.innerHTML = msg;
+    } else if(change > 0) {
+        msg = selectedCandy + ' liberado! Seu troco é de R$' + change;
+
+        totalPaid = 0;
+        change = 0;
+        clearForm();
+        clearTally();
+        messageElement.innerHTML = msg;
+    } else if (totalPaid == 0) {
+        msg = "Por favor! Insira o dinheiro antes de selecionar o doce.";
+        messageElement.innerHTML = msg;
+    } else if(change == 0) {
+        msg = selectedCandy + " dispensado com sucesso!";
+
+        totalPaid = 0;
+        change = 0;
+        clearForm();
+        clearTally();
+        messageElement.innerHTML = msg;
     }
 }
